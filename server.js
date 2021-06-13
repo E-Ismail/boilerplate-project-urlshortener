8fd943d1e5b5922fbd41adc1951771498a494860
echcherrate.ismail <echcherrate.ismail@gmail.com>
2021-06-13 13:21:27 +0100

    shortUrl regex and redirect

--- server.js
+++ server.js
@@ -2,15 +2,25 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 var dns = require('dns');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 const app = express();
+const mongoose = require('mongoose');
+const mongodb = require('mongodb');
+const { response } = require('express');
+const DB_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connection to MongoDB established!')
+  });
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());       // to support JSON-encoded bodies
-app.use(express.urlencoded()); // to support URL-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies 
 
 
 
@@ -25,28 +35,90 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-let short_url = 2;
-let original_url = '';
 
+/* Create URL Model */
+let urlSchema = new mongoose.Schema({
+  original: { type: String, required: true },
+  short: { type: Number }
+})
 
-app.post('/api/shorturl', function (req, res) {
-  original_url = req.body.url;
-  if (original_url.startsWith('https')) {
-    original_url= original_url.replace('https://','');
+let Url = mongoose.model('URL', urlSchema)
+let responseObject = {};
+
+app.post('/api/shorturl', express.urlencoded(), function (req, res) {
+  let original_url = req.body.url;
+  console.log(original_url);
+  let inputUrl = req.body.url
+  responseObject['original_url'] = inputUrl
+  let urlRegex = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi)
+
+  if (!inputUrl.match(urlRegex)) {
+    response.json({ error: 'Invalid URL' })
+    return
   }
-  dns.lookup(original_url, function (err, addresses, family) {
-    console.log('& ' + addresses + 'Family' + family)
-    if (err) {
-      res.json({ error: 'invalid url' });
-    } else {
-      res.json({ "original_url": original_url, "short_url": short_url });
-    }
-  });
+
+  responseObject['original_url'] = inputUrl
+
+  let inputShort = 1
+
+  Url.findOne({})
+    .sort({ short: 'desc' })
+    .exec((error, result) => {
+      if (!error && result != undefined) {
+        inputShort = result.short + 1
+      } else {
+        console.log(error);
+      }
+      if (!error) {
+        console.log('here')
+        console.log(result)
+        Url.findOneAndUpdate(
+          { original: inputUrl },
+          {
+            original: inputUrl,
+            short: inputShort
+          },
+          { new: true, upsert: true, useFindAndModify: true },
+          (error, savedUrl) => {
+            if (!error) {
+              console.log('savedUrl' + savedUrl)
+              responseObject['short_url'] = savedUrl.short
+              res.json(responseObject)
+            } else {
+              console.log(error);
+            }
+          }
+        )
+      } else {
+        console.log(error);
+      }
+    })
 });
 
+
+
+// dns.lookup(original_url, function (err, addresses, family) {
+//   console.log('& ' + addresses + 'Family' + family);
+
+//   if (err) {
+//     res.json({ error: 'invalid url' });
+//   } else {
+//     resObject['original_url'] = original_url;
+//     resObject['short_url'] = short_url;
+//     res.json(resObject);
+//   }
+// });
+
+
 app.get('/api/shorturl/:url_id', function (req, res) {
-  if (req.params.url_id) res.redirect(original_url)
-  else res.json({ error: 'invalid url' });
+  let urlID = req.params.url_id;
+  Url.findOne({ short: urlID }, (error, result) => {
+    if (!error && result != undefined) {
+      res.redirect(result.original);
+    } else {
+      res.json({ error: 'invalid url' });
+    }
+  });
 });
 
 
